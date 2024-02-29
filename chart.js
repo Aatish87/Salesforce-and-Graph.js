f90f91555feb3e8374b0c4a000ebcdbdfb2d8784
Aatish Singh <48213112+Aatish87@users.noreply.github.com>
2024-02-29 20:22:01 +0530

    Update chart.js

--- chart.js
+++ chart.js
@@ -5,26 +5,37 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class Chart extends LightningElement {
 
+    // Public properties that can be set from parent components
     @api loaderVariant = 'base';
     @api chartConfig;
 
+    // Private tracking variable to determine if ChartJS has been initialized
     @track isChartJsInitialized;
+
+    // Callback that is triggered when the component is rendered on the UI
     renderedCallback() {
 
+        // Check if ChartJS is already initialized, if yes, return
         if (this.isChartJsInitialized) {
             return;
         }
 
-        // load static resources.
+        // Load ChartJS script as a static resource
         Promise.all([loadScript(this, chartjs)])
             .then(() => {
+                // Set the flag to indicate ChartJS is initialized
                 this.isChartJsInitialized = true;
+
+                // Get the canvas context and initialize the Chart using the provided configuration
                 const ctx = this.template.querySelector('canvas.barChart').getContext('2d');
                 this.chart = new window.Chart(ctx, JSON.parse(JSON.stringify(this.chartConfig)));
+
+                // Set styles for the chart container
                 this.chart.canvas.parentNode.style.height = 'auto';
                 this.chart.canvas.parentNode.style.width = '80%';
             })
             .catch(error => {
+                // Display an error toast if there is an issue loading ChartJS
                 this.dispatchEvent(
                     new ShowToastEvent({
                         title: 'Error loading ChartJS',
